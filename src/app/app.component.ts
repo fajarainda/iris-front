417c8922d18873197f9e20bec76af26aeac90106
fajarainda <fajardwimaw@gmail.com>
2023-05-19 18:43:48 +0700

    add front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iris-fronted';
+
+  onClick() {
+    console.log("coba")
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary' : '#1ABC9C',
+        'soft-primary' : '#A028ED',
+        'secondary' : '#5CD111',
+        'soft-secondary' : '#75ED28',
+        'error' : '#E6194D',
+        'soft-error' : '#EB4770',
+        'succes' : '#62D862',
+        'soft-succes' : '#8BE28B',
+        'white' : '#FFFFFF',
+        'dark-white' : '#F5F5F5',
+        'soft-gray' : '#E8E9EB',
+        'dark-gray' : '#AAAAAA',
+        'soft-black' : '#212A3F',
+        'light-black': '#263455',
+        'black' : '#20263C'
+        
+      },
+
+      fontFamily: {
+        sans : ['Pitagon']
+      }
+    },
+  },
+  plugins: [],
+}
+
